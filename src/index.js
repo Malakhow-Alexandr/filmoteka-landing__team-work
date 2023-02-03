b9c7e748652a3807f88791a6f8e84e2cfe8af662
RomanKhorol <Rom4uk691@gmail.com>
2023-02-04 01:26:56 +0200

    scrollButtonWitoutFunction

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-import axios from "axios";
-import throttle from "lodash.throttle";
-import debounce from "lodash.debounce";
-
-
+import axios from 'axios';
+import throttle from 'lodash.throttle';
+import debounce from 'lodash.debounce';
 
 // fetch function
 
 import './js/moviesApiService';
 
-
-
-
 // footer
 import './js/footer/footer-heart';
 import './js/footer/footer-modal';
+
+// scroll to top
+import './js/scrollToTop';
