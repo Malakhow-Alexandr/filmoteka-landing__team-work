f96f0fc2c0c6af898e3fcf8f89252365e968339a
DDimbe <109931951+DmytroDatsiuk@users.noreply.github.com>
2023-02-04 16:32:01 +0200

    markup register modal

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-import axios from "axios";
-import throttle from "lodash.throttle";
-import debounce from "lodash.debounce";
-
-
+import axios from 'axios';
+import throttle from 'lodash.throttle';
+import debounce from 'lodash.debounce';
 
 // fetch function
 
 import './js/moviesApiService';
 
-
-
-
 // footer
 import './js/footer/footer-heart';
 import './js/footer/footer-modal';
+
+// register
+
+import './js/account';
