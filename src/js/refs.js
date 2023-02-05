880a9890098b81806ce76b55bf6bdc0fd23abcd8
Yevhenii Sitolenko <110056243+Yevhenii770@users.noreply.github.com>
2023-02-05 15:23:30 +0100

    btn+

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,3 +1,12 @@
 export const refs = {
     gallery: document.querySelector('.gallery'),
+    paginationList: document.querySelector('.pagination'),
+    paginationBtnList: document.querySelector('.pagination__list'),
+    firstPaginationBtn: document.querySelector('#first-btn'),
+    lastPaginationBtn: document.querySelector('#last-btn'),
+    previousArrow: document.querySelector('.js-previous'),
+    nextArrow: document.querySelector('.js-next'),
+    paginationRightDots: document.querySelector('.js-right-dots'),
+    paginationLeftDots: document.querySelector('.js-left-dots'),
+    filmsEl: document.querySelector('.films'),
 }
