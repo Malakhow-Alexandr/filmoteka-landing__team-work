aad5eadbc12e4a047bd797e68920250741f423a2
Kyrya84 <sergii.kyreienko@gmail.com>
2023-02-06 10:48:57 +0200

    add markup slider
    Slider відображається на сторінці. Підключена прокрутка слайдера з клавіатури, та дотиком миші.

--- src/js/slider-trailer.js
+++ src/js/slider-trailer.js
@@ -1,44 +1,97 @@
 import Glide from '@glidejs/glide';
-
-import  MoviesApiService  from './moviesApiService';
-
-// import renderMoviesCard from './renderMoviesGallery';
-// import trailer from '';
-// import { axios } from '';
-import axios from "axios";
-// import { API_KEY, URL } from '../utils/constants';
-
-const API_URL = "https://api.themoviedb.org/3/";
-const API_USER_KEY = "ea73d929c285b7e8f7948351eebc9766";
+import filmsCardSliderTpl from '../templates/card-films-slider.hbs';
+import trailer from './trailer.js';
 
 const sliderContainer = document.querySelector('.js-slider-container');
 renderTrendy();
 
 const glide = new Glide('.glide', {
-  type: 'slider',
+  type: 'carousel',
   startAt: 0,
-  perView: 8,
-  autoplay: 3000,
+  perView: 10,
+  gap: 10,
+  autoplay: 1500,
   hoverpause: true,
+  keyboard: true,
   bound: true,
+  swipeThreshold: false,
+  animationDuration: 800,
+  breakpoints: {
+    768: {
+      perView: 2
+    },
+    480: {
+      perView: 1
+    }
+  }
 });
 
 glide.mount();
 
 function renderTrendy() {
-  try {
-    const response = MoviesApiService();
-    return response
-      .then(({ data }) => {
-        renderSliderFilms(data.results);
-        localStorage.setItem('slider-en', `${JSON.stringify(data.results)}`);
-        axios.get(`${URL}trending/movie/week?api_key=${API_USER_KEY}`).then(({ data }) => { localStorage.setItem('slider', `${JSON.stringify(data.results)}`) });
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  } catch (error) {
-    console.log(error);
-  }
+  const url = `https://api.themoviedb.org/3/trending/all/day?api_key=ea73d929c285b7e8f7948351eebc9766`;
+  return fetch(url)
+    .then(response => response.json())
+    .then(({ results }) => {
+      return results;
+    })
+    .then(renderSliderFilms)
+    .catch(err => {
+      sliderContainer.innerHTML = `<img class="catch-error-pagination" src="${errorUrl}" />`;
+    });
 }
+
+function renderSliderFilms(articles) {
+  sliderContainer.innerHTML = filmsCardSliderTpl(articles);
+  trailer.createTrailerLink(document.querySelectorAll('.btn-youtube-slider'));
+}
+
+
+
+
+
+
+// import Glide from '@glidejs/glide';
+
+// import  MoviesApiService  from './moviesApiService';
+
+// // import renderMoviesCard from './renderMoviesGallery';
+// // import trailer from '';
+// // import { axios } from '';
+// import axios from "axios";
+// // import { API_KEY, URL } from '../utils/constants';
+
+// const API_URL = "https://api.themoviedb.org/3/";
+// const API_USER_KEY = "ea73d929c285b7e8f7948351eebc9766";
+
+// const sliderContainer = document.querySelector('.js-slider-container');
+// renderTrendy();
+
+// const glide = new Glide('.glide', {
+//   type: 'slider',
+//   startAt: 0,
+//   perView: 8,
+//   autoplay: 3000,
+//   hoverpause: true,
+//   bound: true,
+// });
+
+// glide.mount();
+
+// function renderTrendy() {
+//   try {
+//     const response = MoviesApiService();
+//     return response
+//       .then(({ data }) => {
+//         renderSliderFilms(data.results);
+//         localStorage.setItem('slider-en', `${JSON.stringify(data.results)}`);
+//         axios.get(`${URL}trending/movie/week?api_key=${API_USER_KEY}`).then(({ data }) => { localStorage.setItem('slider', `${JSON.stringify(data.results)}`) });
+//       })
+//       .catch(error => {
+//         console.log(error);
+//       });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
