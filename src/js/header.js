c7fc390f0219b2299b64b660a3a38fdb9a23421f
Alice Atamanenko <alice_ataman@icloud.com>
2023-02-06 20:33:41 +0300

    update header

--- src/js/header.js
+++ src/js/header.js
@@ -1,6 +1,23 @@
-const libraryLink = document.querySelector("library");
-libraryLink.addEventListener("click", createLibraryHeader);
+import { refs } from './refs';
+import {clearLibrary} from './watchQueueBtns';
 
-const createLibraryMarkup = () => {
-    return 
-};
+const libraryLink = document.querySelector(".library");
+libraryLink.addEventListener("click", onLibraryClick);
+
+refs={
+navList: document.querySelector(".navigation_list"),
+searchForm: document.querySelector(".header_form"),
+autorization: document.querySelector('.autorization')
+}
+  
+function onLibraryClick(e) {
+    e.preventDefault();
+    navList.classList.add('.hide');
+    searchForm.classList.add('.hide');
+    autorization.classList.add('.hide');
+    
+    refs.btnWatched.classList.add('.show');
+    refs.btnQueue.classList.add('.show');
+
+    clearLibrary();
+  }
