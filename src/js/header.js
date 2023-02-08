32f95e4af9ac7157063911cd93ba977f36e1c349
Alice Atamanenko <alice_ataman@icloud.com>
2023-02-08 19:28:41 +0300

    styles Mylibrary

--- src/js/header.js
+++ src/js/header.js
@@ -1,23 +1,21 @@
 import { refs } from './refs';
-import {clearLibrary} from './watchQueueBtns';
 
-const libraryLink = document.querySelector(".library");
-libraryLink.addEventListener("click", onLibraryClick);
+refs.btnWatched.addEventListener("click", onLibraryClick);
 
-refs={
-navList: document.querySelector(".navigation_list"),
-searchForm: document.querySelector(".header_form"),
-autorization: document.querySelector('.autorization')
+references={
+bcgHeader: document.querySelector(".bcg"),
+searchForm: document.querySelector(".header_form_search"),
+activeLib: document.querySelector('.library'),
+libMenu: document.querySelector('header-lib__menu'),
+// activeBtnWatched: document.querySelector('watched-active')
 }
   
 function onLibraryClick(e) {
     e.preventDefault();
-    navList.classList.add('.hide');
-    searchForm.classList.add('.hide');
-    autorization.classList.add('.hide');
-    
-    refs.btnWatched.classList.add('.show');
-    refs.btnQueue.classList.add('.show');
-
-    clearLibrary();
+    references.bcgHeader.classList.remove('header-bg');
+    references.bcgHeader.classList.add('header-lib');
+    references.libMenu.classList.remove('.hide');
+    references.searchForm.classList.add('.hide');
+    references.activeLib.classList.add('.current');
+    references.btnWatched.classList.add('.watched-active');
   }
