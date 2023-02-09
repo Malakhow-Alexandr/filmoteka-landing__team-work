6efb1994ad7684d3c9412c23b492e2bcf6265c0c
Alice Atamanenko <alice_ataman@icloud.com>
2023-02-09 17:12:31 +0300

    add-dropdown-menu

--- src/js/header.js
+++ src/js/header.js
@@ -1,21 +1,14 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
 import { refs } from './refs';
+// close the toggle menu if user clicks outside of the menu
 
-refs.btnWatched.addEventListener("click", onLibraryClick);
-
-references={
-bcgHeader: document.querySelector(".bcg"),
-searchForm: document.querySelector(".header_form_search"),
-activeLib: document.querySelector('.library'),
-libMenu: document.querySelector('header-lib__menu'),
-// activeBtnWatched: document.querySelector('watched-active')
-}
-  
-function onLibraryClick(e) {
-    e.preventDefault();
-    references.bcgHeader.classList.remove('header-bg');
-    references.bcgHeader.classList.add('header-lib');
-    references.libMenu.classList.remove('.hide');
-    references.searchForm.classList.add('.hide');
-    references.activeLib.classList.add('.current');
-    references.btnWatched.classList.add('.watched-active');
+const profileMenu = document.querySelector('.dropdown-menu');
+profileMenu.addEventListener('click', function(event) {
+  if(
+    $('.toggle > input').is(':checked') && !$(event.target).parents('.toggle').is('.toggle')
+  ) {
+    $('.toggle > input').prop('checked', false);
   }
+})
+
